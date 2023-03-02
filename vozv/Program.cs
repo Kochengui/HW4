b4c13d8426f7be1b33f7c959411f61f956b09f16
Anton <nachalnikkochengui@gmail.com>
2023-03-02 16:44:51 +0800

    V

---
+++ vozv/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int vozvB(int A, int B)
+{
+    int result = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        result = result * A;
+    }
+
+    return result;
+}
+int VozvB = vozvB(A, B);
+Console.WriteLine("Ответ: " + VozvB);
