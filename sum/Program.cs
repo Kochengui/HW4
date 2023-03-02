f237a9e458fdede6fc7a8dc6d24d83438c970051
Anton <nachalnikkochengui@gmail.com>
2023-03-02 17:10:34 +0800

    S

---
+++ sum/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int numberN)
+{
+
+    int counter = Convert.ToString(numN).Length;
+    int coffe = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+        coffe = numN - numN % 10;
+        result = result + (numN - coffe);
+        numN = numN / 10;
+    }
+    return result;
+}
+
+int sumNumber = SumNumber(numN);
+Console.WriteLine("Сумма цифр в числе N : " + sumNumber);
